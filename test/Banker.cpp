f01fb423a1b3f2595c2ede97121449b4a199a532
harkaranbrar <harkaranbrar7@gmail.com>
2017-11-25 20:42:43 -0800

    .

--- test/Banker.cpp
+++ test/Banker.cpp
@@ -84,6 +84,7 @@ void banker::display_data()
 
 
 bool banker::is_it_safe(){
+    
     int myWork[Resources];
     bool finish[Customers];
     string sequence[Customers];
@@ -109,11 +110,51 @@ bool banker::is_it_safe(){
     
     while (loopNumber<10) {
         
-        for(int i=0; i < Resources;i++)
+        for(int i=0; i < Customers;i++)
         {
             need_flag=true;
-        }
+            if (finish[i]==false)
+            {
+                for(int j=0; j<Resources;j++)
+                {
+                    if(needed[i][j]>myWork[j])
+                    {
+                        need_flag = false;
+                        break;
+                    } //end if
+                }//end for
+                while(need_flag == true && finish[i]==false)
+                {
+                    for(int j=0; j<Resources; j++)
+                    {
+                        myWork[j]=myWork[j]+alloc[i][j];
+                    } //end for
+                    
+                    finish[i]=true;
+                    convert<<i;
+                    sequence[num++]="Process"+ convert.str();
+                    convert.str("");
+                }//end while
+            }//end if
+        } //end for
+        loopNumber++;
     } //end while
     
-    return 0;
-}
+    for(int i=0;i<Customers;i++)
+        if(finish[i]==false)
+            safe_flag=false;
+    
+    
+    if(safe_flag)
+    {
+        cout<<"The request is safe and is granted";
+        cout<<"\nSafety Sequence is ";
+        for(int i=0;i<Customers;i++)
+            cout<<sequence[i]<<"->";
+    }
+    else
+        cout<<"The request denied\n";
+    
+    return safe_flag;
+     
+   }
